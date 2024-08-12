cec4790533b39a31e62304a178ea174a6101571e
Nikhil Singh <nikhilbdps1256@gmail.com>
2024-08-12 20:34:28 +0530

    sdf

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import img1 from "../../assets/Hero.png";
 import "./Hero.css";
 
 const Hero = () => {
-  return <div className="home_hero"></div>;
+  return (
+    <div className="home_hero1">
+      <div className="home_hero">
+        <img src={img1} alt="" />
+      </div>
+      <div className="home_hero_buttons">
+        <button>For Colleges</button>
+        <button>For Sponsors</button>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
